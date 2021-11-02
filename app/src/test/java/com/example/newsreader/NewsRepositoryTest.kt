4cad6944bf2b985a37c9f356a93093bef51df201
Jashaswee Jena <j.ishujena2000@gmail.com>
2021-11-02 18:21:04 +0530

    Add tests to verify LocalDataSource and RemoteDataSource working in NewsRepositoryTest

--- app/src/test/java/com/example/newsreader/NewsRepositoryTest.kt
+++ app/src/test/java/com/example/newsreader/NewsRepositoryTest.kt
@@ -17,23 +17,45 @@ class NewsRepositoryTest {
     private val newsRemoteDataSource: DataSource = mockk()
     private val newsLocalDataSource: DataSource = mockk()
     private val onlineChecker: IOnlineChecker = mockk()
+    val article: Article = Article(
+        author = "Author",
+        content = "Content",
+        description = "Description",
+        publishedAt = System.currentTimeMillis().toString(),
+        source = Source(name = "Source", id = "123"),
+        title = "Title",
+        url = "https://www.ndr.de/nachrichten/niedersachsen/braunschweig_harz_goettingen/VW-Krise-Konzernchef-Diess-kuendigt-Jobabbau-in-Wolfsburg-an,vw5706.html",
+        urlToImage = "https://www.ndr.de/nachrichten/niedersachsen/braunschweig_harz_goettingen/volkswagen2082_v-contentxl.jpg",
+        id = 1L
+    )
+
     private val repository = NewsRepository(newsLocalDataSource, newsRemoteDataSource, onlineChecker)
 
     @Test
-    fun testGetRemoteNewsArticles() {
-        val currentTime = System.currentTimeMillis()
-        val article: Article = Article(
-            author = "Author",
-            content = "Content",
-            description = "Description",
-            publishedAt = currentTime.toString(),
-            source = Source(name = "Source", id = "123"),
-            title = "Title",
-            url = "https://www.ndr.de/nachrichten/niedersachsen/braunschweig_harz_goettingen/VW-Krise-Konzernchef-Diess-kuendigt-Jobabbau-in-Wolfsburg-an,vw5706.html",
-            urlToImage = "https://www.ndr.de/nachrichten/niedersachsen/braunschweig_harz_goettingen/volkswagen2082_v-contentxl.jpg",
-            id = 1L
-            )
-        var articlesObservables = Single.just(listOf(article))
+    fun `test remote data source when local is empty and device is offline`() {
+        val articlesObservables = Single.just(listOf(article))
+
+        every { newsLocalDataSource.getArticles() } returns Single.just(listOf())
+        every { onlineChecker.isOnline() } returns false
+        every { newsLocalDataSource.saveArticles(listOf(article)) } returns Unit
+
+        // Test data validation
+        every { newsRemoteDataSource.getArticles() } returns articlesObservables
+
+        var output = repository.getArticles().test()
+        output.assertComplete()
+        output.assertNoErrors()
+        assertEquals(listOf(article), output.values().first())
+
+        // Test error cases
+        every { newsRemoteDataSource.getArticles() } returns Single.error(Exception())
+        output = repository.getArticles().test()
+        output.assertError(Exception::class.java)
+    }
+
+    @Test
+    fun `test remote data source device is online and local is empty`() {
+        val articlesObservables = Single.just(listOf(article))
 
         every { newsLocalDataSource.getArticles() } returns Single.just(listOf())
         every { onlineChecker.isOnline() } returns true
@@ -51,6 +73,27 @@ class NewsRepositoryTest {
         every { newsRemoteDataSource.getArticles() } returns Single.error(Exception())
         output = repository.getArticles().test()
         output.assertError(Exception::class.java)
+    }
 
+
+    @Test
+    fun `test fetch from local database when device is offline and local is not empty`() {
+        val articlesObservables = Single.just(listOf(article))
+
+        every { newsLocalDataSource.getArticles() } returns articlesObservables
+        every { onlineChecker.isOnline() } returns false
+        every { newsLocalDataSource.saveArticles(listOf(article)) } returns Unit
+
+        every { newsRemoteDataSource.getArticles() } returns Single.just(listOf(article))
+
+        var output = repository.getArticles().test()
+        output.assertComplete()
+        output.assertNoErrors()
+        assertEquals(listOf(article), output.values().first())
+
+        // Test error cases
+        every { newsLocalDataSource.getArticles() } returns Single.error(Exception())
+        output = repository.getArticles().test()
+        output.assertError(Exception::class.java)
     }
 }
