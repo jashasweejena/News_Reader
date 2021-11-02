8d1519609c2615095c832eb773a863f1816f4ecf
Jashaswee Jena <j.ishujena2000@gmail.com>
2021-11-02 18:31:40 +0530

    NewsRepositoryTest: Make commonly used variables global

--- app/src/test/java/com/example/newsreader/NewsRepositoryTest.kt
+++ app/src/test/java/com/example/newsreader/NewsRepositoryTest.kt
@@ -17,7 +17,7 @@ class NewsRepositoryTest {
     private val newsRemoteDataSource: DataSource = mockk()
     private val newsLocalDataSource: DataSource = mockk()
     private val onlineChecker: IOnlineChecker = mockk()
-    val article: Article = Article(
+    private val article: Article = Article(
         author = "Author",
         content = "Content",
         description = "Description",
@@ -29,15 +29,19 @@ class NewsRepositoryTest {
         id = 1L
     )
 
+    private val articleTwo: Article = article.copy(id = 2L)
+    private val articlesList = listOf(article, articleTwo)
+
+    private val articlesObservables = Single.just(articlesList)
+
     private val repository = NewsRepository(newsLocalDataSource, newsRemoteDataSource, onlineChecker)
 
     @Test
     fun `test remote data source when local is empty and device is offline`() {
-        val articlesObservables = Single.just(listOf(article))
 
         every { newsLocalDataSource.getArticles() } returns Single.just(listOf())
         every { onlineChecker.isOnline() } returns false
-        every { newsLocalDataSource.saveArticles(listOf(article)) } returns Unit
+        every { newsLocalDataSource.saveArticles(articlesList) } returns Unit
 
         // Test data validation
         every { newsRemoteDataSource.getArticles() } returns articlesObservables
@@ -45,7 +49,7 @@ class NewsRepositoryTest {
         var output = repository.getArticles().test()
         output.assertComplete()
         output.assertNoErrors()
-        assertEquals(listOf(article), output.values().first())
+        assertEquals(articlesList, output.values().first())
 
         // Test error cases
         every { newsRemoteDataSource.getArticles() } returns Single.error(Exception())
@@ -55,11 +59,10 @@ class NewsRepositoryTest {
 
     @Test
     fun `test remote data source device is online and local is empty`() {
-        val articlesObservables = Single.just(listOf(article))
 
         every { newsLocalDataSource.getArticles() } returns Single.just(listOf())
         every { onlineChecker.isOnline() } returns true
-        every { newsLocalDataSource.saveArticles(listOf(article)) } returns Unit
+        every { newsLocalDataSource.saveArticles(articlesList) } returns Unit
 
         // Test data validation
         every { newsRemoteDataSource.getArticles() } returns articlesObservables
@@ -67,7 +70,7 @@ class NewsRepositoryTest {
         var output = repository.getArticles().test()
         output.assertComplete()
         output.assertNoErrors()
-        assertEquals(listOf(article), output.values().first())
+        assertEquals(articlesList, output.values().first())
 
         // Test error cases
         every { newsRemoteDataSource.getArticles() } returns Single.error(Exception())
@@ -78,18 +81,17 @@ class NewsRepositoryTest {
 
     @Test
     fun `test fetch from local database when device is offline and local is not empty`() {
-        val articlesObservables = Single.just(listOf(article))
 
         every { newsLocalDataSource.getArticles() } returns articlesObservables
         every { onlineChecker.isOnline() } returns false
-        every { newsLocalDataSource.saveArticles(listOf(article)) } returns Unit
+        every { newsLocalDataSource.saveArticles(articlesList) } returns Unit
 
         every { newsRemoteDataSource.getArticles() } returns Single.just(listOf(article))
 
         var output = repository.getArticles().test()
         output.assertComplete()
         output.assertNoErrors()
-        assertEquals(listOf(article), output.values().first())
+        assertEquals(articlesList, output.values().first())
 
         // Test error cases
         every { newsLocalDataSource.getArticles() } returns Single.error(Exception())
